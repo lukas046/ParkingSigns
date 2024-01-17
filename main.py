d34b9597f173aa6784b3c4f69d722de6e50b75f2
lca162 <lucaschen688@gmail.com>
2024-01-16 21:19:13 -0800

    created image filter

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import tensorflow
 import os
 import imghdr
+import cv2
+from matplotlib import pyplot as plt
+data_dir = 'data'
+os.listdir(data_dir)
+image_exts = ['jpeg', 'jpg','bmp','png']
+image_exts[2]
+for image_class in os.listdir(data_dir):
+    for image in os.listdir(os.path.join(data_dir,image_class)):
+        image_path = os.path.join(data_dir,image_class, image)
+        try:
+            img = cv2.imread(image_path)
+            tip = imghdr.what(image_path)
+            if tip not in image_exts:
+                print("Image not in ext list")
+                os.remove(image_path)
+        except Exception as e:
+            print("issue")
+
+                    
